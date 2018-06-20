8b7bffbb80e01aff0504f8493cce4dbd5e2d9498
unknown <elmo.visuri@gmail.com>
2018-06-20 11:43:14 +0300

    Added to github

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hei Elmo!");
         System.out.println("Näkyykö tämä?");
+        System.out.println("Tämä pistetään githubiin");
     }
 }
